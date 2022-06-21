f58a555b0326ce5447726b9ec8c56756a11bb895
Teng Fu <ifuteng@gmail.com>
2022-06-21 23:28:04 +0800

    stop broker first, then bookie, at last is ZK

--- stop-all.sh
+++ stop-all.sh
@@ -5,14 +5,14 @@
 # Permission to copy and modify is granted under the Apache 2.0 license
 # Last revised 23/3/2022
 
-pulsar-1/bin/pulsar-daemon stop zookeeper
-pulsar-2/bin/pulsar-daemon stop zookeeper
-pulsar-3/bin/pulsar-daemon stop zookeeper
+pulsar-1/bin/pulsar-daemon stop broker
+pulsar-2/bin/pulsar-daemon stop broker
+pulsar-3/bin/pulsar-daemon stop broker
 
 pulsar-1/bin/pulsar-daemon stop bookie
 pulsar-2/bin/pulsar-daemon stop bookie
 pulsar-3/bin/pulsar-daemon stop bookie
 
-pulsar-1/bin/pulsar-daemon stop broker
-pulsar-2/bin/pulsar-daemon stop broker
-pulsar-3/bin/pulsar-daemon stop broker
+pulsar-1/bin/pulsar-daemon stop zookeeper
+pulsar-2/bin/pulsar-daemon stop zookeeper
+pulsar-3/bin/pulsar-daemon stop zookeeper
